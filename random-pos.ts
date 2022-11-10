08905add0dedb296f3e597be903cef21b3083e90
rakibrr <37996283+rakibrr@users.noreply.github.com>
2022-11-10 19:42:27 +0600

    sdfs

--- random-pos.ts
+++ random-pos.ts
@@ -2,20 +2,24 @@ namespace sprites {
     /**
      * Sets a random Position for the sprite
      */
-    //% blockId=setRndPos block="set $sprite=variables_get to a random Position on the screen"
+    //% blockId=setRndPos block="set $sprite=variables_get to a random Position every $seconds seconds"
     //% group="Random Place"
     //% weight=10
     //% blockGap=8
-    export function setRndPos(sprite: Sprite) {
+    export function setRndPos(sprite: Sprite, seconds: number) {
 
-        let screenWidth = scene.screenWidth()
-        let screenHeight = scene.screenHeight()
+let ms = seconds * 1000
+game.onUpdateInterval(ms, function() {
+    let screenWidth = scene.screenWidth()
+    let screenHeight = scene.screenHeight()
 
-        let randx = randint(0, screenWidth)
-        let randy = randint(0, screenHeight)
+    let randx = randint(0, screenWidth)
+    let randy = randint(0, screenHeight)
 
-        sprite.x = randx
-        sprite.y = randy
+    sprite.x = randx
+    sprite.y = randy
+})
+        
     }
 
 }
